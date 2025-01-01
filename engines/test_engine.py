4d6d229ec81d25c404b3b07e6e36368e06bfaaae
Kevin Kamto <kamtokevin@gmail.com>
2025-01-01 16:01:59 -0500

    chore: removed empty engines

--- engines/test_engine.py
+++
@@ -1,9 +0,0 @@
-class TestEngine:
-    def __init__(self):
-        self.name = "TestEngine"
-
-    def gather_context(self):
-        return {"test_key": "test_value"}
-
-    def run(self):
-        return "TestEngine is running!"
